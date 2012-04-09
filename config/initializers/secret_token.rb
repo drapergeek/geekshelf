acc47da77b7da9e9bb1664472a16f69ef9fa092d
Jason Draper <jason@drapergeek.com>
2012-04-09 13:48:53 -0400

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geekshelf::Application.config.secret_token = '4d34292dea823d432c653e0bf2d9b902b71daaf5c7edc0db8328c93cdef12ce36384cea608da9e22041c33df969411871261429229df10b5576fcae35ef0e56f'
